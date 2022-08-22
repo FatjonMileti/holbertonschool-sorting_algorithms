4d698f95205afa39857b377e3e3068928647c169
FatjonMileti <tonimileti@yahoo.com>
2022-08-22 04:40:07 -0700

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ *selection_sort - sort array
+ *@array: array
+ *@size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size -1; i++)
+	{
+		size_t min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array,size);
+		}
+	}
+}
