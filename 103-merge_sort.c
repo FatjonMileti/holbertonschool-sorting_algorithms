906b95316725a9f4cfd1dcc2a0debf7e880669c7
FatjonMileti <tonimileti@yahoo.com>
2022-08-29 01:22:54 -0700

    ...

---
+++ 103-merge_sort.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+/**
+ *
+ */
+
+void merge_sort(int *array, size_t size)
+{
+	int *temp;
+
+	temp = malloc(sizeof( *temp) * size);
+	if (temp == NULL)
+		return;
+
+	if (array == NULL || size < 2)
+		return;
+	sort(array,temp, size);
+	free(temp);
+}
+void sort(int *array, int *temp, size_t size)
+{
+	size_t i, j, k, mid;
+
+	mid = size / 2;
+	i = 0;
+       	j = 0;
+
+	if (size < 2)
+		return;
+	sort(array, temp, mid);
+	sort(array + mid, temp + mid, size - mid);
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array, mid);
+	printf("[right]: ");
+	print_array(array + mid, size - mid);
+
+	for(k = 0; k < size; k++)
+	{
+		if (j >= size - mid || (i < mid && array[i] < (array + mid)[j]))
+		{
+			temp[k] = array[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = (array + mid)[j];
+			j++;
+		}
+	}
+	for(k = 0; k < size; k++)
+	{
+		array[k] = temp[k];
+	}
+	
+	printf("[Done]: ");
+	print_array(array, size);
+}
